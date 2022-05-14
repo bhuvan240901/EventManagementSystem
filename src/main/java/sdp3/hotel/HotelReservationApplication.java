49b5587daab3065ca4a8c4d349edb658a839639e
PEDDIREDDY VENKATA SIVA DURGA REDDY <190031290cse@gmail.com>
2022-05-15 02:31:46 +0530

    done

---
+++ src/main/java/sdp3/hotel/HotelReservationApplication.java
@@ -0,0 +1,14 @@
+package sdp3.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelReservationApplication.class, args);
+		System.out.println("Server is Running");
+	}
+
+}
